aca660c3e6e282dc34fa18633a4e79eedabeafbc
nwolfson22-cyber <nwolfson22@gmail.com>
2026-05-09 21:30:52 +0000

    Initial commit

---
+++ src/FactDisplay.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FactDisplay = () => {
+  return (
+    <div className="fact-display">
+      
+        <p className="prompt-text">🌟 Press the button below to view a fun fact about me! 🌟</p>
+      
+    </div>
+  );
+};
+
+export default FactDisplay;
