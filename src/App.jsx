db7b770715344448f6a96e1ec423c70ecb010e75
nwolfson22-cyber <nwolfson22@gmail.com>
2026-05-09 22:02:12 +0000

    start2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import WelcomeHeader from './WelcomeHeader';'./App.css';
 
 function App() {
   return (
---
+++ src/WelcomeHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomeHeader = () => {
+    return (
+        <>
+<header className="app-header">
+    <h1>My Fun Facts</h1>
+    <p>Random Personal Facts Generator</p>
+</header>        
+        </>
+    );
+};
